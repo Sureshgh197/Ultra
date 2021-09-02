6fea365bf1980df2c3013e71ef84d75649714524
SURESH BATTA <61498234+Sureshgh197@users.noreply.github.com>
2021-09-02 11:09:04 +0530

    Update settings.py
    secret removed

--- Ultra/settings.py
+++ Ultra/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@qqhg7@lzx351oqsk8j7m4ujhdwf%6^%6cm9*+4^6scu4vmn4v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
